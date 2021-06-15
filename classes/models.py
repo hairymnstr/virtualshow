836e88c0f90eb9f6b4fe341a72c978ee0fd28b7d
Nathan Dumont <nathan@nathandumont.com>
2021-06-15 12:05:03 +0100

    Only try rotate if the file has an orientation key

--- classes/models.py
+++ classes/models.py
@@ -100,21 +100,21 @@ class EntryImage(models.Model):
         
         for orientation in ExifTags.TAGS.keys():
             if ExifTags.TAGS[orientation]=='Orientation':
+                try:
+                    exif=dict(image._getexif().items())
+                    
+                    if exif[orientation] == 3:
+                        image = image.rotate(180, expand=True)
+                    elif exif[orientation] == 6:
+                        image = image.rotate(270, expand=True)
+                    elif exif[orientation] == 8:
+                        image = image.rotate(90, expand=True)
+                except AttributeError:
+                    # no Exif tags on this image
+                    pass
+        
                 break
-        
-        try:
-            exif=dict(image._getexif().items())
-            
-            if exif[orientation] == 3:
-                image = image.rotate(180, expand=True)
-            elif exif[orientation] == 6:
-                image = image.rotate(270, expand=True)
-            elif exif[orientation] == 8:
-                image = image.rotate(90, expand=True)
-        except AttributeError:
-            # no Exif tags on this image
-            pass
-        
+
         image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
     
         temp = BytesIO()
